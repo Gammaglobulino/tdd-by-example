8ae24cdf51bfb9acdaf1400b609006f85e789e49
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-06 15:39:22 +0200

    To simple Franc and Dollar objects created with tests

--- src/main/java/guru/springframework/Money.kt
+++ src/main/java/guru/springframework/Money.kt
@@ -1,9 +1,29 @@
 package guru.springframework
 
 class Dollar(var amount:Int=0){
-    fun amount()=amount
+    private fun amount()=amount
     fun times(multiplier:Int):Dollar{
         return Dollar(amount*multiplier)
     }
+    override fun equals(other: Any?):Boolean {
+        return when(other){
+            is Dollar -> other.amount== amount
+            else -> false
+        }
 
+    }
+
+}
+class Franc(var amount:Int=0){
+    private fun amount()=amount
+    fun times(multiplier:Int):Franc{
+        return Franc(amount*multiplier)
+    }
+    override fun equals(other: Any?):Boolean {
+        return when(other){
+            is Franc -> other.amount== amount
+            else -> false
+        }
+
+    }
 }
--- src/test/java/guru/springframework/MoneyTest.kt
+++ src/test/java/guru/springframework/MoneyTest.kt
@@ -7,12 +7,30 @@ import org.junit.jupiter.api.Assertions.*
 internal class MoneyTest {
 
     @Test
-    fun testMultiplication() {
+    fun testMultiplicationDollar() {
         val five = Dollar(5)
         var product=five.times(2)
-        assertEquals(10,product.amount)
+        assertEquals(Dollar(10),product)
         product=five.times(3)
-        assertEquals(15,product.amount)
+        assertEquals(Dollar(15),product)
+    }
+    @Test
+    fun testEqualityDollar(){
+        assertEquals(Dollar(5),Dollar(5))
+        assertNotEquals(Dollar(5),Dollar(3))
+    }
+    @Test
+    fun testMultiplicationFranc() {
+        val five = Franc(5)
+        var product=five.times(2)
+        assertEquals(Franc(10),product)
+        product=five.times(3)
+        assertEquals(Franc(15),product)
+    }
+    @Test
+    fun testEqualityFranc(){
+        assertEquals(Franc(5),Franc(5))
+        assertNotEquals(Franc(5),Franc(3))
     }
 
 }
